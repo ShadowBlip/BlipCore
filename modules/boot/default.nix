0b0e588ae2ad317b50ea60aa91c411c3e8811c02
William Edwards <shadowapex@gmail.com>
2025-02-27 22:50:55 -0800

    move boot module and add hid-steam patch

---
+++ modules/boot/default.nix
@@ -0,0 +1,64 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+
+let
+  hid-steam = pkgs.callPackage ../../pkgs/by-name/hi/hid-steam/package.nix {
+    # Make sure the module targets the same kernel as system is using.
+    kernel = config.boot.kernelPackages.kernel;
+  };
+in
+
+{
+  # Bootloader.
+  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
+  boot.loader.systemd-boot.enable = lib.mkDefault true;
+  boot.loader.systemd-boot.configurationLimit = lib.mkDefault 10;
+
+  # Plymouth
+  boot.consoleLogLevel = lib.mkDefault 0;
+  boot.initrd.verbose = lib.mkDefault false;
+  boot.loader.timeout = lib.mkDefault 0;
+  boot.plymouth = lib.mkDefault {
+    enable = true;
+    theme = "rings";
+    themePackages = with pkgs; [
+      # By default we would install all themes
+      (adi1090x-plymouth-themes.override {
+        selected_themes = [ "rings" ];
+      })
+    ];
+  };
+
+  # Kernel
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+  boot.kernelParams = lib.mkDefault [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
+
+  boot.extraModulePackages = [
+    hid-steam
+  ];
+
+  # Kernel Configuration
+  # https://discourse.nixos.org/t/patching-an-in-tree-linux-kernel-module/22137/2
+  # https://nixos.wiki/wiki/Linux_kernel#Custom_configuration
+  # https://nixos.org/manual/nixos/unstable/index.html#sec-kernel-config
+  #nixpkgs.config.packageOverrides = pkgs: pkgs.lib.recursiveUpdate pkgs {
+  #  linuxKernel.kernels.linux_5_10 = pkgs.linuxKernel.kernels.linux_5_10.override {
+  #    extraConfig = ''
+  #      KGDB y
+  #    '';
+  #  };
+  #};
+
+}
--- modules/default.nix
+++ modules/default.nix
@@ -5,49 +5,9 @@
 }:
 
 {
-  # Bootloader.
-  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
-  boot.loader.systemd-boot.configurationLimit = lib.mkDefault 10;
-
-  # Plymouth
-  boot.consoleLogLevel = lib.mkDefault 0;
-  boot.initrd.verbose = lib.mkDefault false;
-  boot.loader.timeout = lib.mkDefault 0;
-  boot.plymouth = lib.mkDefault {
-    enable = true;
-    theme = "rings";
-    themePackages = with pkgs; [
-      # By default we would install all themes
-      (adi1090x-plymouth-themes.override {
-        selected_themes = [ "rings" ];
-      })
-    ];
-  };
-
-  # Kernel
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
-  boot.kernelParams = lib.mkDefault [
-    "quiet"
-    "splash"
-    "boot.shell_on_fail"
-    "loglevel=3"
-    "rd.systemd.show_status=false"
-    "rd.udev.log_level=3"
-    "udev.log_priority=3"
+  imports = [
+    ./boot
   ];
-
-  # Kernel Configuration
-  # https://discourse.nixos.org/t/patching-an-in-tree-linux-kernel-module/22137/2
-  # https://nixos.wiki/wiki/Linux_kernel#Custom_configuration
-  # https://nixos.org/manual/nixos/unstable/index.html#sec-kernel-config
-  #nixpkgs.config.packageOverrides = pkgs: pkgs.lib.recursiveUpdate pkgs {
-  #  linuxKernel.kernels.linux_5_10 = pkgs.linuxKernel.kernels.linux_5_10.override {
-  #    extraConfig = ''
-  #      KGDB y
-  #    '';
-  #  };
-  #};
 
   # Networking
   networking.hostName = lib.mkDefault "nixos"; # Define your hostname.
