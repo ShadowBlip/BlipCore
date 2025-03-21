c44cfb4778d49e28564751e15bd91385ab4485b4
William Edwards <shadowapex@gmail.com>
2025-03-20 20:05:57 -0700

    add sysctl settings from jovian

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -61,4 +61,21 @@ in
   #  };
   #};
 
+  boot.kernel.sysctl = {
+    # From steamos-customizations-jupiter
+    # https://github.com/Jovian-Experiments/steamos-customizations-jupiter/commit/bc978e8278ca29a884b1fe70cf2ec9e1e4ba05df
+    "kernel.sched_cfs_bandwidth_slice_u" = lib.mkDefault 3000;
+    "kernel.sched_latency_ns" = lib.mkDefault 3000000;
+    "kernel.sched_min_granularity_ns" = lib.mkDefault 300000;
+    "kernel.sched_wakeup_granularity_ns" = lib.mkDefault 500000;
+    "kernel.sched_migration_cost_ns" = lib.mkDefault 50000;
+    "kernel.sched_nr_migrate" = lib.mkDefault 128;
+    "kernel.split_lock_mitigate" = lib.mkDefault 0;
+
+    # > This is required due to some games being unable to reuse their TCP ports
+    # > if they're killed and restarted quickly - the default timeout is too large.
+    #  - https://github.com/Jovian-Experiments/steamos-customizations-jupiter/commit/4c7b67cc5553ef6c15d2540a08a737019fc3cdf1
+    "net.ipv4.tcp_fin_timeout" = lib.mkDefault 5;
+  };
+
 }
