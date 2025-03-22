aa6ce56d4c329c5ddd6c4a5ee7c91f7e97b8ada2
William Edwards <shadowapex@gmail.com>
2025-03-22 00:10:24 -0700

    add udev rule to do turbo takeover for onexplayer

--- modules/devices/onexplayer/boot.nix
+++ modules/devices/onexplayer/boot.nix
@@ -22,9 +22,15 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    # Configure the panel orientation for the console and plymouth
     boot.kernelParams = lib.mkOverride 100 [
       "fbcon=rotate:3"
       "video=eDP-1:panel_orientation=left_side_up"
     ];
+
+    # Add a udev rule to enable takeover of the turbo button
+    services.udev.extraRules = lib.mkDefault ''
+      KERNEL=="oxp-platform", SUBSYSTEM=="platform", RUN="/run/current-system/sw/bin/sh -c 'echo 1 > /sys/bus/platform/devices/oxp-platform/tt_toggle'", TAG+="uaccess"
+    '';
   };
 }
